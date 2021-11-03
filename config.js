eeb50c8006371edc2a3d8c906bb559339918a9c0
Ky43eRR8 <71791513+Ky43eRR8@users.noreply.github.com>
2021-11-03 23:01:40 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const TOKEN = "8d62e787a156cd14fcd34fcbc3c24d25bbcbf57cbb41fc2934824561ab9bad40904f00b717468ea13c839"; 
 const GROUP_ID = 208552861; 
-const BTN_TEXT = ["vkbot.ru", "vkbot.ru"];
+const BTN_TEXT = ["vto.pe", "vto.pe"];
 const TIME = 300;
-const CHAT_SPAM = ["卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐"];
-const HELLO_TEXT = `deadrose bot.
+const CHAT_SPAM = ["卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐"];
+const HELLO_TEXT = ЛЮДИ ВАМ ВСЕМ ПИЗДЕЦ! @ALL @ALL.
 `;
 
 module.exports = {
