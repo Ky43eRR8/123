4c8eb1f17d8ccfc0fb3b72a42b5bfa4f1cd49d6b
Ky43eRR8 <71791513+Ky43eRR8@users.noreply.github.com>
2021-11-03 22:56:31 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const TOKEN = "8d62e787a156cd14fcd34fcbc3c24d25bbcbf57cbb41fc2934824561ab9bad40904f00b717468ea13c839"; 
 const GROUP_ID = 208552861; 
-const BTN_TEXT = ["STOP                                        vto.pe", "STOP                           vkbot.ru", "STOP                                          vkbot.ru", "STOP                           vkbot.ru", "STOP                           vkbot.ru", "STOP                           vkbot.ru", "STOP                           vkbot.ru", "STOP                           vkbot.ru", "STOP                           vkbot.ru", "STOP                           vkbot.ru", "STOP                           vkbot.ru"];
+const BTN_TEXT = ["vkbot.ru", "vkbot.ru"];
 const TIME = 300;
 const CHAT_SPAM = ["卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐"];
 const HELLO_TEXT = `deadrose bot.
